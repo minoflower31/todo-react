a03f5cbe94542f7567eda8b5409c8803bcb73727
mhpark <minoflower@i-heart.co.kr>
2024-04-19 12:36:17 +0900

    feat: useReducer 를 활용한 AppV2 추가

---
+++ client/src/AppV2.jsx
@@ -0,0 +1,83 @@
+import { useReducer, useRef, useState } from "react";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import TodoEditor from "./components/TodoEditor.jsx";
+import TodoList from "./components/TodoList.jsx";
+
+const mockData = [
+  {
+    id: 1,
+    isDone: true,
+    content: "React 공부하기",
+    createdAt: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "Spring boot 공부하기",
+    createdAt: new Date().getTime(),
+  },
+  {
+    id: 3,
+    isDone: true,
+    content: "Kotlin 공부하기",
+    createdAt: new Date().getTime(),
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE": {
+      return [...state, action.data];
+    }
+    case "UPDATE": {
+      return state.map((it) =>
+        it.id === action.data ? { ...it, isDone: !it.isDone } : it,
+      );
+    }
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.data);
+    }
+  }
+}
+
+function AppV2() {
+  const [todos, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: ++idRef.current,
+        isDone: false,
+        content,
+        createdAt: new Date().getTime(),
+      },
+    });
+  };
+
+  const onUpdate = (targetId) => {
+    dispatch({
+      type: "UPDATE",
+      data: targetId,
+    });
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      data: targetId,
+    });
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <TodoEditor onCreate={onCreate} />
+      <TodoList todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+    </div>
+  );
+}
+
+export default AppV2;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import AppV2 from "./AppV2.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<AppV2 />);
