3fac99211ece24ccb0a301a8ad4f5887a874f422
mhpark <minoflower@i-heart.co.kr>
2024-04-19 13:04:54 +0900

    feat: todo 전체 건수, 완료 건수, 미완료 건수 표시 및 useMemo로 최적화

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import "./TodoList.css";
 import TodoItem from "./TodoItem.jsx";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export default function TodoList({ todos, onUpdate, onDelete }) {
   const [search, setSearch] = useState("");
@@ -19,23 +19,49 @@ export default function TodoList({ todos, onUpdate, onDelete }) {
     );
   };
 
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("called analyzedTodoData()");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
   return (
     <div className="TodoList">
-      <h4>Todos</h4>
+      <div className="todo-header">
+        <h4>Todos</h4>
+        <div className="todo-state">
+          <div>
+            전체 <span>{totalCount}</span>
+          </div>
+          <div>
+            완료 <span>{doneCount}</span>
+          </div>
+          <div>
+            미완료 <span>{notDoneCount}</span>
+          </div>
+        </div>
+      </div>
       <div className="search-wrapper">
         <input
-            value={search}
-            onChange={onChangeSearch}
-            placeholder="검색어를 입력하세요."
+          value={search}
+          onChange={onChangeSearch}
+          placeholder="검색어를 입력하세요."
         />
         <i className="fa fa-search icon"></i>
       </div>
       <div className="todos_wrapper">
         {filterTodos().map((todo) => (
-            <TodoItem
-                key={todo.id}
-                todo={todo}
-                onUpdate={onUpdate}
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onUpdate={onUpdate}
             onDelete={onDelete}
           />
         ))}
