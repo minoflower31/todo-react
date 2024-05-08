507833f26a3df0d70e711ea8a6f60853d964e9d3
mhpark <minoflower@i-heart.co.kr>
2024-05-08 14:19:05 +0900

    feat: 전체 건수, 완료 건수, 미완료 건수 기능에 대해 누락되어 다시 추가

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,7 +1,7 @@
 import "./TodoList.css";
 import TodoItem from "./TodoItem.jsx";
-import { useContext, useState } from "react";
-import { TodoStateContext } from "../TodoContext.jsx";
+import {useContext, useMemo, useState} from "react";
+import {TodoStateContext} from "../TodoContext.jsx";
 
 export default function TodoList() {
   //구조분해할당으로 받아선 안됨
@@ -20,26 +20,52 @@ export default function TodoList() {
     }
 
     return todos.filter((todo) =>
-      todo.content.toLowerCase().includes(search.toLowerCase()),
+        todo.content.toLowerCase().includes(search.toLowerCase()),
     );
   };
 
+  const {totalCount, doneCount, notDoneCount} = useMemo(() => {
+    console.log("called analyzedTodoData()");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
   return (
-    <div className="TodoList">
-      <h4>Todos</h4>
-      <div className="search-wrapper">
-        <input
-          value={search}
-          onChange={onChangeSearch}
-          placeholder="검색어를 입력하세요."
-        />
-        <i className="fa fa-search icon"></i>
+      <div className="TodoList">
+        <div className="todo-header">
+          <h4>Todos</h4>
+          <div className="todo-state">
+            <div>
+              전체 <span>{totalCount}</span>
+            </div>
+            <div>
+              완료 <span>{doneCount}</span>
+            </div>
+            <div>
+              미완료 <span>{notDoneCount}</span>
+            </div>
+          </div>
+        </div>
+        <div className="search-wrapper">
+          <input
+              value={search}
+              onChange={onChangeSearch}
+              placeholder="검색어를 입력하세요."
+          />
+          <i className="fa fa-search icon"></i>
+        </div>
+        <div className="todos_wrapper">
+          {filterTodos().map((todo) => (
+              <TodoItem key={todo.id} todo={todo}/>
+          ))}
+        </div>
       </div>
-      <div className="todos_wrapper">
-        {filterTodos().map((todo) => (
-          <TodoItem key={todo.id} todo={todo} />
-        ))}
-      </div>
-    </div>
   );
 }
